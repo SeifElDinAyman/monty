d93063eb8a903390c97a6ee2e25e61e5dc9f4318
Seif El-Din <SeifElDinAyman77@gmail.com>
2023-03-23 22:58:05 +0200

    MSG

--- monty_main.c
+++ monty_main.c
@@ -1,9 +1,15 @@
 #include "monty.h"
+#include <stdio.h>
+#define _GNU_SOURCE
+#include <stdlib.h>
+
 bus_t bus = {NULL, NULL, NULL, 0};
+
 /**
-* main - monty code interpreter
-* @argc: number of arguments
-* @argv: monty file location
+* main - function for monty code interpreter
+* @argc: argument count
+* @argv: argument value
+*
 * Return: 0 on success
 */
 int main(int argc, char *argv[])
@@ -30,7 +36,7 @@ int main(int argc, char *argv[])
 	while (read_line > 0)
 	{
 		content = NULL;
-		//read_line = getline(&content, &size, file);
+		read_line = getline(&content, &size, file);
 		bus.content = content;
 		counter++;
 		if (read_line > 0)
